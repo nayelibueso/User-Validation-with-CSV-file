60205a21188ce038320f9656891b59414378e9fc
nayelibueso <nayelibueso50@gmail.com>
2024-03-19 17:43:07 -0500

    Get comfortable
    deleted whole class and starting form scratch. Code compiled, working on muscle memory..

--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -12,53 +12,7 @@ import java.io.IOException;
 //This class is meant to handle user-related functionalities like loading users from a file and validating user credentials.
 
 	public class UserService {
-		private User[] users; // import class
 
-		public UserService(String fileName) {
-			users = new User[4];// Initializing the users array with a size of 4
-			loadUsersFromFile(fileName);
-		}
-
-		public void loadUsersFromFile(String fileName) {
-			BufferedReader fileReader = null;
-			try {
-				fileReader = new BufferedReader(new FileReader(fileName));
-				String line;
-				int index = 0;
-				while ((line = fileReader.readLine()) != null) {
-					String[] parts = line.split(",");
-					String username = parts[0];
-					String password = parts[1];
-					String name = parts[2];
-					users[index] = new User(username, password, name);
-					index++;
-				}
-
-			} catch (FileNotFoundException e) {
-				System.out.println("oops, there was an exception: File not found");
-				e.printStackTrace();
-			} catch (IOException e) {
-				System.out.println("oops, there was an I/O Exception");
-				e.printStackTrace();
-			} finally {
-				try {
-					if (fileReader != null) {
-						fileReader.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		public User validateUser (String username, String password) {
-			for (User user : users) {
-				if (user != null && user.getPassword().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
-					return user;
-				}
-			}
-			return null;
-		}
 	}
 
 
